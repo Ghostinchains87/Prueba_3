3fce2df24f8c32927119de67ee993fdee8a7c8e8
Ghostinchains87 <bru.miranda@duocuc.cl>
2024-06-22 10:30:57 -0400

    Se añaden comentarios y se modifica el menu de opciones

--- import csv.py
+++ import csv.py
@@ -1,19 +1,22 @@
 import csv
 
+#Funcion para leer el archivo CSV
 def leer_archivo():
     with open(r"C:\Users\bmiranda\Prueba_3\asistencia_alumnos.csv", newline='') as archivo:
         reader = csv.reader(archivo, delimiter=";")
         return list(reader)
-
+    
+#Funcion para consultar la asistencia de los alumnos
 def consultar_asistencia(rut, datos):
     for row in datos:
         if row[1] == rut:
-            total_clases = int(row[5]) + int(row[6]) + int(row[7])
-            asistencia = (int(row[5]) / total_clases) *100
+            total_clases = int(row[5]) + int(row[6]) + int(row[7]) #Se suman los valores de las filas
+            asistencia = (int(row[5]) / total_clases) *100 #Se divide el valor "presente" por el total de las clases
             print(f"La asistencia actual de {row[3]}, {row[4]} es: {asistencia}%")
             return
     print("Rut no encontrado")
-
+    
+#Funcion para visualizar la asistencia baja de los alumnos
 def visualizar_asistencia_baja(datos):
     for row in datos:
         total_clases = int(row[5]) + int(row[6]) + int(row[7])
@@ -21,6 +24,7 @@ def visualizar_asistencia_baja(datos):
         if asistencia < 70:
             print(f"{row[3]}, {row[4]} tiene una asistencia actual de: {asistencia}%")
 
+#Funcion para visualizar la baja asistencia
 def visualizar_asistencia_baja_seccion(datos, seccion):
     contador = 0
     for row in datos:
@@ -31,6 +35,7 @@ def visualizar_asistencia_baja_seccion(datos, seccion):
                 contador += 1
     print(f"Hay {contador} alumnos con asistencia actual menor al 70% en la sección {seccion}")
 
+#Funcion para generar un archivo de salida con los datos obtenidos
 def generar_archivo_salida(datos, seccion):
     with open(f"asistencia_{seccion}.csv", "w", newline="") as archivo:
         writer = csv.writer(archivo, delimiter=";")
@@ -41,10 +46,11 @@ def generar_archivo_salida(datos, seccion):
                 asistencia = (int(row[5]) / total_clases) *100
                 writer.writerow([row[1], row[3], row[4], asistencia])
 
+#Funcion para salir del programa
 def salir_del_programa():
     print("Hasta luego!")
     
-    
+#Funcion donde se almacena el menu de opciones del programa   
 def menu_principal():
     datos = leer_archivo()
     opciones = {
@@ -54,7 +60,12 @@ def menu_principal():
         "4": "Generar archivo de salida de alumnos con asistencia en una sección",
         "5": "Salir del Programa"
     }
-    opcion = input("Seleccione una opción (1-5): ")
+    opcion = input("""1: Consultar asistencia actual de un alumno 
+2: Visualizar alumnos con asistencia menor al 70%
+3: Visualizar número de alumnos con asistencia menor al 70% en una sección
+4: Generar archivo de salida de alumnos con asistencia en una sección
+5: Salir del Programa
+:""")
     while opcion not in opciones:
         print("Opción incorrecta, vuelva a intentarlo.")
         opcion = input("Seleccione una opción (1-5): ")
